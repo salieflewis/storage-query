ffd2c666f83ca5c70b12d00d683e8a8477095f47
Salief <salieflewis@gmail.com>
2023-08-21 19:49:09 -0400

    return concat

--- index.ts
+++ index.ts
@@ -1,14 +1,21 @@
-import { type Block } from 'viem'
-import { client } from './viem/client'
+import { type Block, toHex, concat, type Hex } from 'viem';
+import { client } from './viem/client';
 
-/**
- *  Write some viem things here
- * */
-let latestBlock: Block
+async function queryStorageSlot() {
+  // Logic
+  const logic = await client.getStorageAt({
+    address: '0xF297a0c06F4784a3CEf81C76D3dc1440046db7fE', // press proxy
+    slot: toHex(156),
+  });
+  console.log('Logic:', logic);
+  // Renderer
+  const renderer = await client.getStorageAt({
+    address: '0xF297a0c06F4784a3CEf81C76D3dc1440046db7fE', // press proxy
+    slot: toHex(157),
+  });
+  console.log('Renderer:', renderer);
 
-client.watchBlocks({
-  onBlock: (block) => {
-    console.log(block)
-    latestBlock = block
-  },
-})
+  console.log('Hex concat result:', concat([logic as Hex, renderer as Hex]));
+}
+
+queryStorageSlot();
